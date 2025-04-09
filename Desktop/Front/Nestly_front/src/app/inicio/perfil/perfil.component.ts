aba76afdf8c3fcacabff29ba8a1dd49452ed5384
Juan David Tapia Frias <juandavidtapia123@gmail.com>
2025-04-09 12:15:24 -0600

    TODAS LAS IMAGENES CAMBIADAS CORRECTAMENTE

--- Desktop/Front/Nestly_front/src/app/inicio/perfil/perfil.component.ts
+++ Desktop/Front/Nestly_front/src/app/inicio/perfil/perfil.component.ts
@@ -1,5 +1,7 @@
+import { DashboardComponent } from './../dashboard/dashboard.component';
 import { Component } from '@angular/core';
 import { HttpLavavelService } from '../../http.service';
+import { Usuario } from '../../interface/usuario.interface';
 
 @Component({
   selector: 'app-perfil',
@@ -7,6 +9,9 @@ import { HttpLavavelService } from '../../http.service';
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
+  users:Usuario[]=[];
+  datos:any;
+  total:number = 0;
   constructor(private Shttp: HttpLavavelService){
     this.Shttp.Service_Get('user').subscribe((data)=>{
       console.log(data);
@@ -19,4 +24,25 @@ export class PerfilComponent {
 
     window.location.href = '/login'; 
   }
+
+  ngOnInit(): void {
+    this.obtenerData(); // Llama al método al inicializar el componente
+  }
+
+   obtenerData(){
+    this.Shttp.Service_Get('user').subscribe((res:any)=>{
+      console.log(res);
+      if (res.data) {
+        this.users = res.data;
+        this.total = res.total;
+        console.log('users',this.users);
+        
+      }
+    })
+  } 
+
+
+
+
+
 }
---
+++ Desktop/Front/Nestly_front/src/app/interface/usuario.interface.ts
@@ -0,0 +1,8 @@
+export interface Usuario{
+first_name:string;
+last_name_paternal:string;
+last_name_maternal:string;
+phone: number;
+email:string;
+created_at:string;
+}
